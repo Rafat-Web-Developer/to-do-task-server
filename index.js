b188956dab744e633d2364d6171e33d473db7646
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-07-01 21:33:11 +0600

    Get and Post api create

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
 const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://todoTaskUser1:tOy72LG5m4d9MOQu@cluster0.nyaiy.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const tasksCollection = client.db("todoTaskDB").collection("tasks");
+
+    // Task API
+
+    app.get("/allTasks/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const totalData = await tasksCollection.find(query);
+      res.send(totalData);
+    });
+
+    app.post("/brand/:email", async (req, res) => {
+      const task = req.body;
+      const result = await tasksCollection.insertOne(task);
+      res.send({ success: true, result });
+    });
+  } finally {
+  }
+}
+run();
 
 app.get("/", (req, res) => {
   res.send("Todo Server Working... Alhamdulillah!!");
