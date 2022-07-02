70fe7c10a6ca1f96e9fd2ded595c4ce20f791b22
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-07-02 07:24:41 +0600

    Fix a simple bug in get method

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://todoTaskUser1:tOy72LG5m4d9MOQu@cluster0.nyaiy.mongodb.net/?retryWrites=true&w=majority`;
+const uri =
+  "mongodb+srv://todoTaskUser1:tOy72LG5m4d9MOQu@cluster0.vpsz1.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -23,14 +24,14 @@ async function run() {
 
     // Task API
 
-    app.get("/allTasks/:email", async (req, res) => {
+    app.get("/tasks/:email", async (req, res) => {
       const email = req.params.email;
       const query = { email: email };
-      const totalData = await tasksCollection.find(query);
+      const totalData = await tasksCollection.find(query).toArray();
       res.send(totalData);
     });
 
-    app.post("/brand/:email", async (req, res) => {
+    app.post("/tasks", async (req, res) => {
       const task = req.body;
       const result = await tasksCollection.insertOne(task);
       res.send({ success: true, result });
